863f02d671c8cc1f084d9fa47b6957e5a7db93fe
Brooke Kullberg <brookekullberg@gmail.com>
2022-12-02 15:28:15 -0800

    add project

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<ItemTag> JoinEntities { get;}
+  }
+}
